bb6b42fab9ee155b7972f0f3e4d5f121c70b331b
TangBiJing <787740644@qq.com>
2019-05-17 16:55:47 +0800

    fix:confgig

--- src/main/java/com/wsmhz/api/gateway/config/WebConfig.java
+++ src/main/java/com/wsmhz/api/gateway/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.wsmhz.api.gateway.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * Created By TangBiJing On 2019/4/2
  * Description:
  */
+@Order(Integer.MAX_VALUE)
 @Configuration
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
